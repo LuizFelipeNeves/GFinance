135709547ad5e7c0492990aa842709a3498058d6
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2026-03-09 16:18:07 -0300

    Deploy front

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -e
 
+echo "API_URL: $API_URL"
+
 # Substitui a variável de ambiente no nginx.conf
 if [ -n "$API_URL" ]; then
     sed -i "s|\${API_URL}|$API_URL|g" /etc/nginx/conf.d/default.conf
+    echo "Nginx config updated"
+else
+    echo "WARNING: API_URL is not set!"
 fi
 
 exec nginx -g 'daemon off;'
